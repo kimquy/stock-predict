d137a6fead182d179a48ec4e87416f6e45c9056d
Long Nguyen <nguy1461@vandals.uidaho.edu>
2012-09-18 11:11:20 -0700

    update kimquy.rb

--- kimquy.rb
+++ kimquy.rb
@@ -5,13 +5,19 @@
 require 'json'
 require 'net/http'
 
-uri = URI 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=bagder&&count=1'
+uri = URI 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=bagder&&count=10'
 content = Net::HTTP.get(uri)
 
 json = JSON content
 
+puts ""
+
 json.each { |j|
 	j.each { |key, value|
-		puts value if key == "text"
+		puts "#{value} \n\n" if key == "text"
+		
 	}
 }
+
+
+
