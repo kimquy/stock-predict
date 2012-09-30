88fe95cb683bd7d46a9986ac6bda9c922ea1268e
Long Nguyen <nguy1461@vandals.uidaho.edu>
2012-09-30 13:17:15 -0700

    new kimquy

--- kimquy.rb
+++ kimquy.rb
@@ -6,7 +6,7 @@ require 'rubygems'
 require 'json'
 require 'net/http'
 
-uri = URI 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=bagder&&count=10'
+uri = URI 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=google&&count=10'
 content = Net::HTTP.get(uri)
 
 json = JSON content
@@ -15,6 +15,6 @@ myfile = File.open("tweets.txt", 'w+')
 
 json.each { |j|
 	j.each { |key, value|
-		myfile.write(value) if key == "text"
+		myfile.write("#{value} \n") if key == "text"
 	}
 }
